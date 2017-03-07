489ed33abb23e41408fbad5b65e1089a32c910bf
liangshuai0222 <liangshuai0222@126.com>
2017-03-07 09:00:17 +0800

    first commit

---
+++ LearnGit/src/learncom/LearnCommit.java
@@ -0,0 +1,13 @@
+package learncom;
+
+/**
+* @description 
+* @author Mr.Liang
+* @date 2017年3月7日 上午8:53:40
+*/
+
+public class LearnCommit {
+	public void test(){
+		System.out.println("第一次提交");
+	}
+}

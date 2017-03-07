c279d75194252eb514a746a9bcad17c6b7a908c0
liangshuai0222 <liangshuai0222@126.com>
2017-03-07 14:41:17 +0800

    不懂啊

--- LearnGit/src/learncom/Learn.java
+++ LearnGit/src/learncom/Learn.java
@@ -1,28 +1,25 @@
 package learncom;
 
 /**
-* @description 
-* @author Mr.Liang
-* @date 2017年3月6日 下午11:16:24
-*/
+ * @description
+ * @author Mr.Liang
+ * @date 2017年3月6日 下午11:16:24
+ */
 
 public class Learn {
-	public void test1(){
+	public void test1() {
 		System.out.println("第一次提交");
 	}
-	public void test2(){
+
+	public void test2() {
 		System.out.println("第2次提交");
 	}
-	public void test3(){
+
+	public void test3() {
 		System.out.println("第3次提交");
 	}
-<<<<<<< Updated upstream
-  public void testMain(){
-    	System.out.println("第4次提交");
-  }
-=======
-	public void testMain(){
-		System.err.println("第四次提交");
+
+	public void testMain() {
+		System.out.println("第4次提交");
 	}
->>>>>>> Stashed changes
 }
